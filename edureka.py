6a7cb781237300bdafd15246948eba5b43cfca43
CDAC IOT <cdac.iotlab@gmail.com>
2018-05-04 10:28:15 +0530

    All commands added

--- edureka.py
+++ edureka.py
@@ -1,1 +1,17 @@
 print("Welcome to CDAC !")
+
+
+# mkdir edureka-repo
+# cd edureka-repo
+# git init
+# touch edureka.py
+# gedit edureka.py   ->>>    print("Welcome to CDAC !")
+# git add edureka.py 
+# git status
+# git commit -m "First Commit"
+# git remote add origin "https://github.com/DeveshCdac/Git-Tutorials-DevOps.git"
+# git pull origin master --allow-unrelated-histories
+# ls
+# git pull origin master --allow-unrelated-histories
+# check your GitHub
+
